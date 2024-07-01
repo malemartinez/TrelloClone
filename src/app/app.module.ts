d3e6199b86a82522e8fa7cc4fada96209052e97a
malemartinez <martinezmayra605@gmail.com>
2024-07-01 11:10:51 -0500

    manejo de overlays con CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {OverlayModule} from '@angular/cdk/overlay';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BtnComponent } from './components/btn/btn.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BoardComponent } from './pages/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    BtnComponent
+    BtnComponent,
+    NavbarComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OverlayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
