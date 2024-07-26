baae2aa19fdf1cdd966788e9a79a788f2ee74a1f
malemartinez <martinezmayra605@gmail.com>
2024-07-26 17:56:29 -0500

    Modificaciones a nivel de estilo para el componente boton y el overlay

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -17,12 +17,12 @@ export class BtnComponent implements OnInit {
 
   get colors() {
     const colorsList: { [key:string] : string} = {
-      success: 'bg-success-700 hover:bg-success-800 focus:ring-success-300',
-      danger: 'bg-red-700 hover:bg-red-800 focus:ring-red-300',
-      primary: 'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300',
-      yellow: 'bg-yellow-700 hover:bg-yellow-800 focus:ring-yellow-300',
+      success: 'bg-success-700 hover:bg-success-800 focus:ring-success-300 text-white',
+      danger: 'bg-red-700 hover:bg-red-800 focus:ring-red-300 text-gray-700',
+      primary: 'bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 text-black',
+      yellow: 'bg-yellow-700 hover:bg-yellow-800 focus:ring-yellow-300 text-gray-900',
       sky: 'bg-sky-700 hover:bg-sky-800 focus:ring-sky-300',
-      graylight: 'bg-gray-200 hover:bg-gray-600 focus:ring-gray-300'
+      graylight: 'bg-gray-200 hover:bg-gray-300 focus:ring-gray-300 text-gray-800'
     };
     return colorsList[this.btnColor];
   }
